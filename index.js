bfc4b1fafea801e3ad1209cd59259a5153582ea1
0xsign <0xsign@protonmail.com>
2022-08-26 11:43:45 +0200

    js package

---
+++ index.js
@@ -0,0 +1,19 @@
+const avax = require("./avax/tokenlist.json");
+const bsc = require("./bsc/tokenlist.json");
+const celo = require("./celo/tokenlist.json");
+const ethereum = require("./ethereum/tokenlist.json");
+const fantom = require("./fantom/tokenlist.json");
+const harmony = require("./harmony/tokenlist.json");
+const polygon = require("./polygon/tokenlist.json");
+const xdai = require("./xdai/tokenlist.json");
+
+module.exports = {
+  avax,
+  bsc,
+  celo,
+  ethereum,
+  fantom,
+  harmony,
+  polygon,
+  xdai,
+};
