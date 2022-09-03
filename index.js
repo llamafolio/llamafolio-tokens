abbeac33204638fe7d1268925eaa325e3d273b09
0xsign <0xsign@protonmail.com>
2022-09-03 12:31:40 +0200

    export getToken

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const harmony = require("./harmony/tokenlist.json");
 const polygon = require("./polygon/tokenlist.json");
 const xdai = require("./xdai/tokenlist.json");
 
-module.exports = {
+const chains = {
   avax,
   bsc,
   celo,
@@ -16,4 +16,31 @@ module.exports = {
   harmony,
   polygon,
   xdai,
+};
+
+const registries = {};
+for (const chain in chains) {
+  registries[chain] = {};
+
+  for (const token of chains[chain]) {
+    registries[chain][token.address] = token;
+  }
+}
+
+/**
+ *
+ * @param {string} chain
+ * @param {string} address lowercase hex string. ex: "0x0000000000000000000000000000000000000000"
+ */
+function getToken(chain, address) {
+  if (!registries[chain]) {
+    console.error(`Chain '${chain}' not supported yet`);
+    return;
+  }
+  return registries[chain][address];
+}
+
+module.exports = {
+  getToken,
+  chains,
 };
