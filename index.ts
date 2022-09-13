efe4c7bee54e622cba8f8deb9552495ce4b6a7ee
0xsign <0xsign@protonmail.com>
2022-09-13 18:46:52 +0200

    use typescript

---
+++ index.ts
@@ -0,0 +1,56 @@
+import avax from "./avax/tokenlist.json";
+import bsc from "./bsc/tokenlist.json";
+import celo from "./celo/tokenlist.json";
+import ethereum from "./ethereum/tokenlist.json";
+import fantom from "./fantom/tokenlist.json";
+import harmony from "./harmony/tokenlist.json";
+import polygon from "./polygon/tokenlist.json";
+import xdai from "./xdai/tokenlist.json";
+
+export interface Token {
+  address: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  coingeckoId: string | null;
+  native?: boolean;
+}
+
+export interface ChainToken extends Token {
+  chain: string;
+}
+
+export type ChainTokenRegistry = { [key: string]: ChainToken };
+
+export const chains: { [chain: string]: Token[] } = {
+  avax,
+  bsc,
+  celo,
+  ethereum,
+  fantom,
+  harmony,
+  polygon,
+  xdai,
+};
+
+const registries: { [chain: string]: ChainTokenRegistry } = {};
+
+for (const chain in chains) {
+  const registry: ChainTokenRegistry = (registries[chain] = {});
+
+  for (const token of chains[chain]) {
+    registry[token.address] = { ...token, chain };
+  }
+}
+
+/**
+ * @param chain
+ * @param address lowercase hex string. ex: "0x0000000000000000000000000000000000000000"
+ */
+export function getToken(chain: string, address: string) {
+  if (!registries[chain]) {
+    console.error(`Chain '${chain}' not supported yet`);
+    return;
+  }
+  return registries[chain][address];
+}
