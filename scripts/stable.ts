f311a5dfb299e358d6d8f34c7fc0f85ffd78d3a6
Anton Kondratev <kondratyev.work@gmail.com>
2022-11-11 12:20:10 +0700

    Add a new script that adds and tags all stablecoins from DefiLlama

--- scripts/stable.ts
+++ scripts/stable.ts
@@ -1,6 +1,11 @@
 import fs from "fs";
 import fetch from "node-fetch";
-import { chains } from "../index";
+import childProcess from "child_process"
+
+import path from "path";
+import { coingeckoPlatformToChain } from './add-token';
+
+const supportedChains = Object.values(coingeckoPlatformToChain)
 
 async function main() {
   const stablecoinsRes = await fetch(
@@ -13,19 +18,42 @@ async function main() {
     peggedAssets.map((asset) => asset.gecko_id)
   );
 
-  for (const chain in chains) {
-    const newTokenList = chains[chain].map((token) => {
+
+  for (const chainId of supportedChains) {
+    const tokenListSrc = path.join(__dirname, "..", chainId, "tokenlist.json");
+    const oldTokenList = JSON.parse(fs.readFileSync(tokenListSrc, "utf8"));
+
+    const allStablesForTheChain = peggedAssets.filter(stable => stable.chains.some(c => c.toLowerCase() === chainId))
+    const missingStables = allStablesForTheChain.filter(stable => oldTokenList.every(token => token.symbol !== stable.symbol))
+
+    const tokenUpdateProcess = missingStables.map(token => {
+      console.log(`> Adding ${token.symbol} for ${chainId}`)
+
+      const process = childProcess.fork(path.join(__dirname, "add-token.ts"), ['coingecko', token.gecko_id])
+
+      return new Promise((res, rej) => {
+        process.on('exit', res)
+        process.on('error', rej)
+      })
+    })
+
+    await Promise.all(tokenUpdateProcess)
+
+    const updatedTokenList = JSON.parse(fs.readFileSync(tokenListSrc, "utf8"));
+
+    const newTokenList = updatedTokenList.map((token) => {
       // if the token is known as stable, mark it.
       // if not, leave it undefined
       if (token.coingeckoId && peggedAssetsGeckoIds.has(token.coingeckoId)) {
         token.stable = true;
+        token.wallet = true;
       }
       return token;
     });
 
     fs.writeFileSync(
-      `./${chain}/tokenlist.json`,
-      JSON.stringify(newTokenList, null, 2)
+      `./${chainId}/tokenlist.json`,
+      `${JSON.stringify(newTokenList, null, 2)}\n`
     );
   }
 }
