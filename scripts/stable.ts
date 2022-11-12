6b6395f9a5950164368097cab80508ba9a46c4bb
0xsign <0xsign@protonmail.com>
2022-11-12 07:52:27 +0100

    set back stable script

--- scripts/stable.ts
+++ scripts/stable.ts
@@ -1,11 +1,6 @@
 import fs from "fs";
 import fetch from "node-fetch";
-import childProcess from "child_process"
-
-import path from "path";
-import { coingeckoPlatformToChain } from './add-token';
-
-const supportedChains = Object.values(coingeckoPlatformToChain)
+import { chains } from "../index";
 
 async function main() {
   const stablecoinsRes = await fetch(
@@ -18,41 +13,18 @@ async function main() {
     peggedAssets.map((asset) => asset.gecko_id)
   );
 
-
-  for (const chainId of supportedChains) {
-    const tokenListSrc = path.join(__dirname, "..", chainId, "tokenlist.json");
-    const oldTokenList = JSON.parse(fs.readFileSync(tokenListSrc, "utf8"));
-
-    const allStablesForTheChain = peggedAssets.filter(stable => stable.chains.some(chain => chain.toLowerCase() === chainId))
-    const missingStables = allStablesForTheChain.filter(stable => oldTokenList.every(token => token.symbol !== stable.symbol))
-
-    const tokenUpdateProcess = missingStables.map(token => {
-      console.log(`> Adding ${token.symbol} for ${chainId}`)
-
-      const process = childProcess.fork(path.join(__dirname, "add-token.ts"), ['coingecko', token.gecko_id])
-
-      return new Promise((res, rej) => {
-        process.on('exit', res)
-        process.on('error', rej)
-      })
-    })
-
-    await Promise.all(tokenUpdateProcess)
-
-    const updatedTokenList = JSON.parse(fs.readFileSync(tokenListSrc, "utf8"));
-
-    const newTokenList = updatedTokenList.map((token) => {
+  for (const chain in chains) {
+    const newTokenList = chains[chain].map((token) => {
       // if the token is known as stable, mark it.
       // if not, leave it undefined
       if (token.coingeckoId && peggedAssetsGeckoIds.has(token.coingeckoId)) {
         token.stable = true;
-        token.wallet = true;
       }
       return token;
     });
 
     fs.writeFileSync(
-      `./${chainId}/tokenlist.json`,
+      `./${chain}/tokenlist.json`,
       `${JSON.stringify(newTokenList, null, 2)}\n`
     );
   }
