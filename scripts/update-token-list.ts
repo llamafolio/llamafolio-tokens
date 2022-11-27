82e5088f296146559629060456e4b1a455695172
0xsign <0xsign@protonmail.com>
2022-11-27 13:19:27 +0100

    add aave-usdt

--- scripts/update-token-list.ts
+++ scripts/update-token-list.ts
@@ -1,10 +1,19 @@
 import fs from "fs";
 
 export function updateTokenList(chain: string, tokens: any[]) {
-  tokens.sort((a, b) => a.symbol.localeCompare(b.symbol));
+  const tokenByAddress: { [key: string]: any } = {};
+
+  for (const token of tokens) {
+    token.address = token.address.toLowerCase();
+    tokenByAddress[token.address] = token;
+  }
+
+  const uniqueTokens = Object.values(tokenByAddress);
+
+  uniqueTokens.sort((a, b) => a.symbol.localeCompare(b.symbol));
 
   return fs.writeFileSync(
     `./${chain}/tokenlist.json`,
-    `${JSON.stringify(tokens, null, 2)}\n`
+    `${JSON.stringify(uniqueTokens, null, 2)}\n`
   );
 }
